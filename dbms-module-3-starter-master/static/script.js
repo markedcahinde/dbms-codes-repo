2344baec9d044f393035db64738ac2b10efae060
Mark Oliver Cahinde <m.cahinde@gmail.com>
2015-08-13 12:18:36 +0800

    Revert "Second commit"
    This reverts commit a4328517d75caeea2cbec4385913ab24898d2ace.

--- dbms-module-3-starter-master/static/script.js
+++ dbms-module-3-starter-master/static/script.js
@@ -1,34 +1,25 @@
-function onForm(event){
-  var data = $(event.target).serializeArray();
+function onForm(event) {
+    var data = $(event.target).serializeArray();
+    var thesis = {}
 
-  var thesis = {};
-  for (var i = 0; i < data.length; i++) {
-    thesis[data[i].name] = data[i].value;
+    for (var i = 0; i <= data.length; i++) {
+      thesis[data[i].name] = data[i].value;
+    }
+
+    var thesis_api = '/api/thesis';
+    $.post(thesis_api, thesis, function(response) {
+      if (response.status = 'OK') {
+        var list_element = response.data.thesis_year + ' ' + response.data.thesis_title
+        $('.thesis_list').prepend('<li>' + list_element + '</li>');
+        //$('.field').val(''); //clear fields
+      } else {
+        alert('Something went wrong');
+      }
+    });
+
+    return false;
   }
 
-  var thesis_api = '/api/thesis';
-  $.post(thesis_api, thesis, function(response) {
-    if (response.status = 'OK') {
-      var list_element = response.data.year + ' ' + response.data.title;
-      $('.thesis_list').prepend('<li>' + list_element + '</li>');
-      $('.field').val(''); //clear fields
-    } else {
-      alert('Something went wrong');
-    }
-  });
-  return false;
-}
-
-function loadAll() {
-  var thesis_api = '/api/thesis';
-  $.get(thesis_api, {}, function(response) {
-    console.log('thesis_list', response)
-    response.data.forEach(function(thesis) {
-      var list_element = thesis.year + ' ' + thesis.title;
-      $('.thesis_list').prepend('<li>' + list_element + '</li>');
-    });
-  });
-}
+    // your code here
 
 $('.thesis-entry').submit(onForm);
-loadAll();
